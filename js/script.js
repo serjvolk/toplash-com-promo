8f4c557e223dfb88c18dc8df5f2cbb3ffd9ecb67
sv0ne <serjvolk222@gmail.com>
2022-04-06 13:51:33 +0300

    06.04.2022

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('.js-block-08-slider').slick({
+		prevArrow: $('.block-08__btn'),
+		nextArrow: $('.block-08__btn.arrow-btn_next'),
+		dots: true,
+		fade: true
+	});
+});
